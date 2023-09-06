383ad689fe01afc9afe3cfdc8c5b2f93bb5f1a18
Fausto Sacco <fsaccopadron@gmail.com>
2023-09-06 09:57:56 +0100

    Implementing plus and equals functions

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+let firstNumber;
+let secondNumber;
+
+function plus() {
+  firstNumber = parseInt(output);
+  clearScreen();
+}
+
+function equal() {
+  secondNumber = parseInt(output);
+  let result = firstNumber + secondNumber;
+  document.getElementById("output").innerHTML = result;
+}
+
+let output = "";
+document.getElementById("output").innerHTML = output;
+
 function updateOutput(number) {
-  document.getElementById("output").innerHTML = number;
+  output = output.concat(number);
+  document.getElementById("output").innerHTML = output;
+}
+
+function clearScreen() {
+  document.getElementById("output").innerHTML = "";
+  output = "";
 }
